7970c676e62746f37ce8826f393964a6877ba551
Paul Daintree <paul.daintree@gmail.com>
2011-10-29 07:55:59 +0100

    Removed default Rails page

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -2,6 +2,11 @@ require 'spec_helper'
 
 describe PagesController do
   render_views
+  before(:each) do
+    #
+     @base_title = "Microposts | "
+    #
+  end
 
   describe "GET 'home'" do
     it "should be successful" do
@@ -11,7 +16,7 @@ describe PagesController do
     it "should have the right title" do
       get 'home'
       response.should have_selector("title",
-                    :content => "Microposts | Home")
+                                    :content => @base_title + "Home")
     end
   end
 
@@ -23,7 +28,7 @@ describe PagesController do
         it "should have the right title" do
       get 'contact'
       response.should have_selector("title",
-                    :content => "Microposts | Contact")
+                                    :content => @base_title + "Contact")
     end
   end
   
@@ -35,8 +40,21 @@ describe PagesController do
         it "should have the right title" do
       get 'about'
       response.should have_selector("title",
-                    :content => "Microposts | About")
+                                    :content => @base_title + "About")
     end
   end
   
+  render_views
+
+  describe "GET 'help'" do
+    it "should be successful" do
+      get 'help'
+      response.should be_success
+    end
+    it "should have the right title" do
+      get 'help'
+      response.should have_selector("title",
+                                    :content => "Microposts | Help")
+    end
+  end
 end
