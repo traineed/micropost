5bd35c12f208479936de7d36723bd84898592021
Paul Daintree <paul.daintree@gmail.com>
2011-11-05 17:08:52 +0000

    User Model with Show Page

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,17 +1,23 @@
 require 'spec_helper'
 
-describe UsersController do
+
+  describe UsersController do
   render_views
 
-  describe "GET 'new'" do
+  describe "GET 'show'" do
+
+    before(:each) do
+      @user = Factory(:user)
+    end
+
     it "should be successful" do
-      get 'new'
+      get :show, :id => @user
       response.should be_success
     end
 
-    it "should have the right title" do
-      get 'new'
-      response.should have_selector("title", :content => "Sign up")
+    it "should find the right user" do
+      get :show, :id => @user
+      assigns(:user).should == @user
     end
   end
 end
